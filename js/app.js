f278063b22901112a6138c449d1c42dacd0cbac4
Krzysztof Kolacz <kolin4@gmail.com>
2017-10-18 15:37:45 +0200

    add multi storage func

--- js/app.js
+++ js/app.js
@@ -1,44 +1,59 @@
-let btn = document.querySelector('button');
-let input = document.querySelector('input[type=text]');
+let btnSave = document.querySelector('#save'); //zapis listy
+let inputNameList = document.querySelector('#listName'); // input nazwy listy
+
+let input = document.querySelector('#task'); // input task
 let tabList =  [];
-let form = document.querySelector('form');
+let form = document.querySelector('.form');
 let listaUl = document.querySelector('.lista');
 
+// Adding new items to list
 form.addEventListener('submit', function(e){
-
     e.preventDefault();
     if (!input.value == "") {tabList.push(input.value)};
     this.reset();
-
     addItems();
-localStorage.setItem('lista', JSON.stringify(tabList));
+    btnSave.disabled = false;
+// localStorage.setItem('lista', JSON.stringify(tabList));
 
 })
-window.onload = () =>{
-    tabList = JSON.parse(localStorage.getItem('lista')) || [];
-    console.log(tabList);
-    if (tabList == []) return;
-    addItems();
-}
 
+
+// window.onload = () =>{
+//     tabList = JSON.parse(localStorage.getItem('lista')) || [];
+//     console.log(tabList);
+//     if (tabList == []) return;
+//     addItems();
+// }
+
+// Add items to ul list
 function addItems(){
     let list = tabList.map( (elem,i) =>{
-        return `<li  class='item'  data-id=${i}>${elem} </li>
+        return `<li class='item' data-id=${i}>${elem} </li>
         `
     })
-
     listaUl.innerHTML = list.join("");
 
 }
+
+
 window.addEventListener('click', function(e){
     let item = e.target;
     if ( item.classList  == 'item'){
         let idToDel = item.dataset.id;
-
         tabList.splice(idToDel, 1);
         item.remove();
         addItems();
-        console.log(tabList);
     }
 
+})
+
+btnSave.addEventListener('click', function(e){
+
+    function list(name,list){
+        this.name = name,
+        this.list = [list]
+    }
+    let listObj = new list(inputNameList.value, tabList);
+    localStorage.setItem(inputNameList.value, tabList);
+    inputNameList.value = "";
 })

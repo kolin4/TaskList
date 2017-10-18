b553528e04165fba05416c68612b5107b8f7b552
Krzysztof Kolacz <kolin4@gmail.com>
2017-10-18 21:35:09 +0200

    working localstorage with refresh

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@ let btnSave = document.querySelector('#save'); //zapis listy
 let inputNameList = document.querySelector('#listName'); // input nazwy listy
 
 let input = document.querySelector('#task'); // input task
-let tabList =  [];
+let tabList =  []; // main array
 let form = document.querySelector('.form');
-let listaUl = document.querySelector('.lista');
+let listaUl = document.querySelector('.lista'); // list of user things to do
 
 // Adding new items to list
 form.addEventListener('submit', function(e){
@@ -13,17 +13,17 @@ form.addEventListener('submit', function(e){
     this.reset();
     addItems();
     btnSave.disabled = false;
-// localStorage.setItem('lista', JSON.stringify(tabList));
+
 
 })
 
-
-// window.onload = () =>{
-//     tabList = JSON.parse(localStorage.getItem('lista')) || [];
-//     console.log(tabList);
-//     if (tabList == []) return;
-//     addItems();
-// }
+// on refresh loading storage list
+window.onload = () =>{
+    for ( let i = 0; i < localStorage.length; i++){
+            tempList.push(localStorage.key(i));
+    }
+    addElementList();
+}
 
 // Add items to ul list
 function addItems(){
@@ -34,8 +34,18 @@ function addItems(){
     listaUl.innerHTML = list.join("");
 
 }
+// functoin adding user lists
+function addElementList(name){
+    let result = document.querySelector('#result');
+    let newTempList = tempList.map( (elem) =>{
+        return `<li class='yourList'>${elem}</li>`
+    })
+    return result.innerHTML = newTempList.join("");
+}
 
 
+
+//events
 window.addEventListener('click', function(e){
     let item = e.target;
     if ( item.classList  == 'item'){
@@ -52,20 +62,17 @@ window.addEventListener('click', function(e){
 
 })
 
+let tempList = []; //temp list for localStorage list
 btnSave.addEventListener('click', function(e){
     if ( inputNameList.value == ""){
         localStorage.setItem('no-name', JSON.stringify(tabList));
+        tempList.push('no-name')
         addElementList('no-name');
     } else {
         localStorage.setItem(inputNameList.value, JSON.stringify(tabList));
+        tempList.push(inputNameList.value);
         addElementList(inputNameList.value);
     }
-
+    console.log(tempList);
     inputNameList.value = "";
 })
-
-
-function addElementList(name){
-    let result = document.querySelector('#result');
-    return result.innerHTML = `<li class='yourList' data-name=${name}>${name}</li>`
-}
